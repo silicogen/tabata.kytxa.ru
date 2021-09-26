4d87420b0df83691c89cc076ba20cc8bfcd5a360
Alexey <alexey.chuprov@gmail.com>
2021-09-26 07:18:02 +0300

    Providers 0

---
+++ src/Providers.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+// import localforage from 'localforage';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'mobx-react';
+// import theme from "css/theme";
+// import Root from "store/Root";
+// import { persist } from 'mst-persist';
+import { observer } from 'mobx-react-lite';
+// import Goer from 'store/Goer';
+// import { useAsync } from "react-async";
+// import forumStore from 'forum/store/index';
+
+
+// async function getRoot() {
+//   const root = Root.create({}, { goer: new Goer() });
+//   await Promise.all([
+//     persist('tabata', root, { storage: localforage, jsonify: false, blacklist: ["ui", "tmp", "selected"] }),
+//     persist(`tabata.ui`, root.ui, { storage: localforage, jsonify: false, blacklist: ["navMenuIsCollapsed"] }),
+//     persist(`tabata.selected`, root.selected, { storage: localforage, jsonify: false })
+//   ]);
+//   return root;
+// }
+
+const Providers: React.FC = ({ children }) => {
+  // const { data, error, isPending } = useAsync({ promiseFn: getRoot })
+  // if (isPending) return <h1>Загрузка...</h1>
+  // if (error) throw error;
+  // if (data) 
+  return <>
+    <Provider
+    // root={data} store={forumStore}
+    >
+      <ThemeProvider
+        theme={{}}
+      >
+        {children}
+      </ThemeProvider>
+    </Provider>
+  </>;
+  return <h1>Whoops!!! Something defaul has happend!!! It was supposed imposible!!!</h1>
+}
+
+export default observer(Providers);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import Providers from 'Providers';
 
 ReactDOM.render(
-    <App />,
+    <Providers>
+        <App />
+    </Providers>,
     document.getElementById('root')
 )
 

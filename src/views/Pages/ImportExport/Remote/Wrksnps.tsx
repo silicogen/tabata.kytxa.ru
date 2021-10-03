356bbd97d8f25243565874bdd2f534a54c34a37a
Alexey <alexey.chuprov@gmail.com>
2021-10-04 00:12:29 +0300

    nicing

--- src/views/Pages/ImportExport/Remote/Wrksnps.tsx
+++ src/views/Pages/ImportExport/Remote/Wrksnps.tsx
@@ -7,15 +7,15 @@ import { css } from "@styled-system/css";
 import { useTheme } from "css/theme";
 
 
-const Wrksnps0 = () => {
+const _Wrksnps = () => {
     const theme = useTheme();
     const remote = useRemote();
-    useEffect(() => {
-        axios
-            .get(`${API_ROUTE}/posts`)
-            .then(response => remote.wrksnps.setItems(response.data.response));
-    }, []);
-    // const selectedCSS = theme.tableRows.selected(() => remote.wrksnps.isSelected(workout));
+    const wrksnps = remote.wrksnps;
+    const effect = () => {
+        axios.get(`${API_ROUTE}/posts`)
+            .then(resp => wrksnps.setItems(resp.data.response));
+    }
+    useEffect(effect, []);
     return <div>
         <table>
             <thead>
@@ -26,19 +26,19 @@ const Wrksnps0 = () => {
                 </tr>
             </thead>
             <tbody>
-                {remote.wrksnps.items.map(wrksnp => (
+                {wrksnps.items.map(wrksnp =>
                     <tr key={wrksnp.id}
-                        onClick={() => remote.wrksnps.toggleSelect(wrksnp)}
-                        css={css(theme.tableRows.selected(() => remote.wrksnps.isSelected(wrksnp)))}
+                        onClick={() => wrksnps.toggleSelect(wrksnp)}
+                        css={css(theme.tableRows.selected(() => wrksnps.isSelected(wrksnp)))}
                     >
                         <td>{wrksnp.id}</td>
                         <td>{wrksnp.title}</td>
                         <td>{wrksnp.content}</td>
                     </tr>
-                ))}
+                )}
             </tbody>
         </table>
 
     </div>
 }
-export const Wrksnps = observer(Wrksnps0);
+export const Wrksnps = observer(_Wrksnps);

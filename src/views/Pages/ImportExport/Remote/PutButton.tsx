b952ed626e4c8a0218df15ded1cce328a93634cf
Alexey <alexey.chuprov@gmail.com>
2021-10-04 10:46:56 +0300

    put in selected

--- src/views/Pages/ImportExport/Remote/PutButton.tsx
+++ src/views/Pages/ImportExport/Remote/PutButton.tsx
@@ -4,25 +4,29 @@ import axios from 'axios'
 import { getSnapshot } from "mobx-state-tree";
 import { useTheme } from 'css/theme';
 import { useRoot } from "store/Root";
+import { useRemote } from "store/Remote";
+import { observer } from "mobx-react";
 
 
 
-export const PutButton: React.FC = () => {
+export const _PutButton: React.FC = () => {
   const theme = useTheme();
   const root = useRoot();
+  const wrksnp = useRemote().wrksnps.selected;
   return <button
     css={css(theme.buttons.primary)}
+    disabled={!wrksnp}
     onClick={async () => {
       const snap = getSnapshot(root);
       const snapStr = JSON.stringify(snap, undefined, 4);
-      await axios.put(`${API_ROUTE}/posts/${"1"}`, {
-        id: 1,
+      await axios.put(`${API_ROUTE}/posts/${wrksnp.id}`, {
         content: snapStr,
-        author_id: 1,
-        title: 'test state 1'
+        title: new Date().toLocaleString()
       })
     }}
   >
-    Update
-  </button>
+    Сохранить
+  </button >
 }
+
+export const PutButton = observer(_PutButton);

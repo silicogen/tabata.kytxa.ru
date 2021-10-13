19fa9051a33411158f03222edf5442fbb841d90f
Alexey <alexey.chuprov@gmail.com>
2021-10-13 12:14:48 +0300

    NavLink to="/login"

--- src/views/Pages/ImportExport/Remote/Page.tsx
+++ src/views/Pages/ImportExport/Remote/Page.tsx
@@ -6,22 +6,24 @@ import { CRUD } from "./CRUD";
 import { useTheme } from "css/theme";
 import { useRemote } from "store/Remote";
 import { observer } from "mobx-react-lite";
+import { NavLink } from "react-router-dom";
 
 const _Page: React.FC = () => {
     const theme = useTheme();
     const remote = useRemote();
-
-
-
     return <div css={css(theme.divs.commonPage)}>
-        <InOut />
-        {remote.inOut.isAuthenticated &&
-            <section css={css(theme.sections.common)}>
-                <h2>Экспорт-импорт на на удалённом сервере</h2>
-                <CRUD />
-                <RenameWrksnp />
-                <Wrksnps />
-            </section>}
+        <section css={css(theme.sections.common)}>
+            <h2>Экспорт-импорт на на удалённом сервере</h2>
+            {remote.inOut.isAuthenticated
+                ? <>
+                    <CRUD />
+                    <RenameWrksnp />
+                    <Wrksnps />
+                </>
+                : <NavLink to="/login" css={css(theme.navLinks.pageNavLink)}>Выполните вход на сервер</NavLink>
+            }
+
+        </section>
     </div>
 }
 
